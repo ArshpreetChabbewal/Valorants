1279ffc61c637fd0e523d7044abca7512e6899d6
Arshpreet Chabbewal <arsh.c321@gmail.com>
2021-12-04 11:11:40 -0500

    Added authentication

---
+++ util/validators.js
@@ -0,0 +1,40 @@
+module.exports.validateRegisterInput = (
+  username,
+  email,
+  password,
+  confirmPassword
+) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  if (email.trim() === "") {
+    errors.username = "Email must not be empty";
+  } else {
+    const regEx = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/;
+    if (!email.match(regEx)) {
+      console.log("email:" + email);
+      errors.email = "Email must be a valid email address";
+    }
+  }
+  if (password === "") {
+    errors.password = "Password must not be empty";
+  } else if (password !== confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1,
+  };
+};
+
+module.exports.validateLoginInput = (username, password) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty";
+  }
+  if (password.trim() === "") {
+    errors.password = "Password must not be empty";
+  }
+};
